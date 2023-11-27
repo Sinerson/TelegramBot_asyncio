9485d518af542112b114af9ed502cc02f2c08ae3
r3tiu <tbp@sv-tel.ru>
2023-11-27 16:21:59 +0300

    + рефакторинг отмены голоса для голосования

--- handlers/poll_handler.py
+++ handlers/poll_handler.py
@@ -3,6 +3,7 @@ from aiogram import Router
 from aiogram.types import PollAnswer
 from db.redis import RedisConnector
 from services.excel_writer import export_to_excel
+from services.other_functions import get_question_for_poll
 
 poll_rt = Router()
 
@@ -10,8 +11,8 @@ poll_rt = Router()
 # Хэндлер для апдейтов на ответы в голосовании
 @poll_rt.poll_answer()
 async def poll_answer_add_processing(poll_answer: PollAnswer) -> None:
-    # Проверим что за апдейт пришел
-    # Голосование за вариант:
+    # Проверим что за апдейт пришел:
+    # ....голосование за вариант:
     if len(poll_answer.option_ids) > 0:
         conn = RedisConnector().create_connection(database=2)
         answer = {"user_id": poll_answer.user.id,
@@ -19,19 +20,18 @@ async def poll_answer_add_processing(poll_answer: PollAnswer) -> None:
                   "poll_id": poll_answer.poll_id,
                   "option_ids": poll_answer.option_ids[0]
                   }
-        # set_result = conn.hset(poll_answer.user.id, mapping=answer)
         set_result = conn.sadd(f"polls:{poll_answer.poll_id}:{poll_answer.option_ids[0]}", poll_answer.user.id)
-    # Или отмена варианта
+
+        # poll_name = RedisConnector().create_connection(database=1).get(poll_answer.poll_id)
+        # poll_data = RedisConnector().create_connection(database=2).hgetall(poll_answer.user.id)
+        # await export_to_excel(data=poll_data, file_name=poll_name)
+    # ....или отмена варианта
     else:
         conn = RedisConnector().create_connection(database=2)
         # для удаления результат голосования найдем количество ключей в которых может быть абонент
-        cnt_keys = len(conn.keys(f"polls:{poll_answer.poll_id}:*"))
-        ic(cnt_keys)
+        cnt_question = len(get_question_for_poll()[1])
         cnt = 0
         # пробежим по всем ключам, и удалим значение из набора ,если оно где-то встречается
-        while cnt <= cnt_keys:
+        while cnt <= cnt_question:
             conn.srem(f"polls:{poll_answer.poll_id}:{cnt}", poll_answer.user.id)
             cnt += 1
-    # poll_name = RedisConnector().create_connection(database=1).get(poll_answer.poll_id)
-    # poll_data = RedisConnector().create_connection(database=2).hgetall(poll_answer.user.id)
-    # await export_to_excel(data=poll_data, file_name=poll_name)
