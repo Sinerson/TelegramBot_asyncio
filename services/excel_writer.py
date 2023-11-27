2b5bd0e8b4f039c54230f272eb39a394eb129a1c
Bogdan_home <bogdan.tabolin@gmail.com>
2023-11-27 21:52:40 +0300

    +

--- services/excel_writer.py
+++ services/excel_writer.py
@@ -12,3 +12,5 @@ async def export_to_excel(data: dict, file_name: str):
     for cell in ws['A'] + ws[1]:
         cell.style = 'Pandas'
     wb.save(f"{file_name}.xlsx")
+
+# df = pd.DataFrame({'points': [25, 12, 15, 14, 19],'assists': [5, 7, 7, 9, 12],'rebounds': [11, 8, 10, 6, 6]})
